09601f85fb66219698678ae78cf6b975fd20426e
Альберт Александров <albert-ugatu@list.ru>
2019-05-03 23:08:54 +0500

    ДЗ9

---
+++ ДЗ9/js/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', function() {
+    
+    let info = document.querySelector('.info-header'),
+        tab = document.querySelectorAll('.info-header-tab'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    hideTabContent(1);
+
+    info.addEventListener('click', function(e) {
+        let target = e.target;
+
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+});
